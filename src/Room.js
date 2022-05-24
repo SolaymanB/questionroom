4e52094e9e8ada01a6cbefa3d417f938f41ede60
SolaymanB <180046863@aston.ac.uk>
2022-05-24 20:27:41 +0100

    Basic question asking features

--- src/Room.js
+++ src/Room.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,61 +6,110 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import ListGroup from "react-bootstrap/ListGroup";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { v4 as uuidv4 } from 'uuid';
-import dayjs from 'dayjs'
-import { db, auth } from './firebase'
+import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
+import { db, auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
-import { onValue } from "firebase/database";
-
+import cx from "classnames";
+import QuestionCreator from "./components/QuestionCreator";
 
 function Room(props) {
-  const [room, setRoom] = useState(null)
-  const [question, setQuestion] = useState('')
+  const [room, setRoom] = useState(null);
+  const [question, setQuestion] = useState("");
+  const [newQuestionMode, setNewQuestionMode] = useState(false);
 
   const [user, loading] = useAuthState(auth);
   let params = useParams();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleCreateRoom() {
     const roomObject = {
-      "roomId": uuidv4(),
-      "passcode": "1234",
-      "hostId": user.uid,
-      "status": "Active",
-      "startedTime": dayjs().format(),
-      "createdUserName": user?.displayName || 'Anonymous',
-      "parentSessionId": null,
-      "endedTime": null,
-      "tag": "Computer Science",
-      "questions": [],
-      "students": []
-    }
+      roomId: uuidv4(),
+      passcode: "1234",
+      hostId: user?.uid,
+      status: "Active",
+      startedTime: dayjs().format(),
+      createdUserName: user?.displayName || "Anonymous",
+      parentSessionId: null,
+      endedTime: null,
+      tag: "Computer Science",
+      questions: [
+        {
+          // Multiple choice question
+          questionId: uuidv4(),
+          hostId: user?.uid,
+          text: "What is your name?",
+          type: "multi",
+          reactionCount: 3,
+          askedTime: dayjs().format(),
+          userName: user?.displayName || "Anonymous",
+          options: [
+            {
+              optionId: uuidv4(),
+              text: "Ayan",
+            },
+            {
+              optionId: uuidv4(),
+              text: "Sully",
+            },
+            {
+              optionId: uuidv4(),
+              text: "Samir",
+            },
+          ],
+          answers: [
+            // {
+            //   answerId: "1234",
+            //   optionId: "1234",
+            //   studentId: "1234",
+            //   answeredTime: "",
+            //   visibility: "private",
+            // },
+          ],
+        },
+      ],
+      members: [],
+    };
     const usersRef = db.ref(`rooms/${roomObject.roomId}`).set(roomObject);
-    navigate(`/rooms/${roomObject.roomId}`)
+    navigate(`/rooms/${roomObject.roomId}`);
+  }
+
+  function handleAnswerMultiQuestion(question, answer) {
+    console.log({ question, answer });
+    // const usersRef = db.ref(`rooms/${room.roomId}/questions/${question.}`).set({
+    //   // Student question
+    //   uid: user.uid,
+    //   text: question,
+    //   type: "free",
+    //   reactionCount: 0,
+    //   askedTime: dayjs().format(),
+    //   userName: user?.displayName || "Anonymous",
+    // });
   }
 
   function postQuestion(e) {
-    e.preventDefault()
+    e.preventDefault();
+    setNewQuestionMode(false);
     const usersRef = db.ref(`rooms/${room.roomId}/questions/${uuidv4()}`).set({
       // Student question
-      "uid": user.uid,
-      "text": question,
-      "type": "free",
-      "reactionCount": 0,
-      "askedTime": dayjs().format(),
-      "userName": user?.displayName || 'Anonymous'
+      uid: user.uid,
+      text: question,
+      type: "free",
+      reactionCount: 0,
+      askedTime: dayjs().format(),
+      userName: user?.displayName || "Anonymous",
     });
-    setQuestion("")
+    setQuestion("");
   }
-
 
   useEffect(() => {
     if (params.roomId) {
-      const room = db.ref(`rooms/${params.roomId}`).on('value', (snap) => {
-        setRoom(snap.val())
-      })
+      const room = db.ref(`rooms/${params.roomId}`).on("value", (snap) => {
+        setRoom(snap.val());
+      });
     }
   }, [params]);
 
@@ -70,67 +119,137 @@ function Room(props) {
     }
   }, [user]);
 
-
   function renderNewRoomCreator() {
-    return <Modal show={isNewRoomMode} backdrop="static" centered>
-      <Modal.Header>
-        <Modal.Title>New Session</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCreateRoom}>
-          Create Room
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    return (
+      <Modal show={isNewRoomMode} backdrop="static" centered>
+        <Modal.Header>
+          <Modal.Title>New Session</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCreateRoom}>
+            Create Room
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function renderNewQuestionCreator() {
+    return (
+      <Modal
+        show={newQuestionMode}
+        centered
+        onHide={() => setNewQuestionMode(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>New Question</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <QuestionCreator
+            roomId={params.roomId}
+            onQuestionSubmit={() => setNewQuestionMode(false)}
+          />
+        </Modal.Body>
+      </Modal>
+    );
   }
 
   function renderRoom() {
     // convert objects into arrays for easy sorting
-    let questions = room?.questions ? Object.keys(room?.questions).map((k) => { return { ...room?.questions[k], roomId: k } }) : null
-    let sortedQuestions = questions ? questions?.sort((a, b) => (dayjs(a.askedTime).isAfter(dayjs(b.askedTime)) ? 1 : -1)) : null
-    return <Col className="p-5 d-flex flex-column">
-      <Row className="mb-5">
-        <Col>
-          <Button size="lg" variant="light">
-            New Question
-          </Button>
-        </Col>
-        <Col className="d-flex justify-content-end">
-          <Button size="lg" variant="light" className="mx-3">
-            Share
-          </Button>
-          <Button size="lg" variant="light">
-            Export
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex flex-column pb-5 mb-5">
-          {sortedQuestions && Object.keys(sortedQuestions).map((key) => {
-            const question = sortedQuestions[key];
-            const isCurrentUserQuestion = question?.uid === user?.uid;
-            return <Card className="mb-3" style={{ width: '500px', marginLeft: isCurrentUserQuestion ? 'auto' : '' }}>
-              <Card.Body className="rounded-lg">
-                <Card.Title as="strong">
-                  {isCurrentUserQuestion ? 'You' : question?.userName}
-                </Card.Title>
-                <Card.Text>
-                  {question.text}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          })}
-        </Col>
-      </Row>
-    </Col>
+    let questions = room?.questions
+      ? Object.keys(room?.questions).map((k) => {
+          return { ...room?.questions[k], roomId: k };
+        })
+      : null;
+    let sortedQuestions = questions
+      ? questions?.sort((a, b) =>
+          dayjs(a.askedTime).isAfter(dayjs(b.askedTime)) ? 1 : -1
+        )
+      : null;
+    return (
+      <Col className="p-5 d-flex flex-column">
+        <Row className="mb-5">
+          <Col>
+            <Button
+              size="lg"
+              variant="light"
+              onClick={() => setNewQuestionMode(true)}
+            >
+              New Question
+            </Button>
+          </Col>
+          <Col className="d-flex justify-content-end">
+            <Button size="lg" variant="light" className="mx-3">
+              Share
+            </Button>
+            <Button size="lg" variant="light">
+              Export
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-flex flex-column pb-5 mb-5">
+            {sortedQuestions &&
+              Object.keys(sortedQuestions).map((key) => {
+                const question = sortedQuestions[key];
+                const isCurrentUserQuestion = question?.uid === user?.uid;
+                return (
+                  <Card
+                    key={key}
+                    className={cx("chatItem mb-3", {
+                      "border-primary": isCurrentUserQuestion,
+                      "border-secondary": !isCurrentUserQuestion,
+                    })}
+                    style={{
+                      width: "500px",
+                      marginLeft: isCurrentUserQuestion ? "auto" : "",
+                    }}
+                  >
+                    <Card.Body className="rounded bg-light">
+                      <Card.Title>
+                        <strong>
+                          {isCurrentUserQuestion ? "You" : question?.userName}
+                        </strong>{" "}
+                        asked a question
+                      </Card.Title>
+                      <Card.Text>{question.text}</Card.Text>
+                      {question?.options?.length && (
+                        <ListGroup className="mb-3">
+                          {question?.options?.map((a) => (
+                            <ListGroup.Item
+                              action
+                              key={a.optionId}
+                              onClick={() =>
+                                handleAnswerMultiQuestion(question, a.optionId)
+                              }
+                            >
+                              {a.text}
+                            </ListGroup.Item>
+                          ))}
+                        </ListGroup>
+                      )}
+
+                      {!isCurrentUserQuestion && question.type === "free" && (
+                        <Button size="sm" variant="secondary">
+                          Answer Question
+                        </Button>
+                      )}
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+          </Col>
+        </Row>
+      </Col>
+    );
   }
 
-  const isNewRoomMode = Boolean(!params?.roomId)
-
+  const isNewRoomMode = Boolean(!params?.roomId);
   return (
     <>
       <Row className="bg-dark h-100 overflow-auto">
+        {renderNewQuestionCreator()}
         {renderNewRoomCreator()}
         {renderRoom()}
       </Row>
@@ -138,8 +257,8 @@ function Room(props) {
         className="position-fixed w-100"
         style={{ bottom: "0px", left: "0px" }}
       >
-        {!isNewRoomMode &&
-          <Form onSubmit={postQuestion}>
+        {!isNewRoomMode && (
+          <Form onSubmit={postQuestion} className="p-5">
             <Form.Group controlId="question-text">
               <Form.Control
                 autoComplete="off"
@@ -152,8 +271,7 @@ function Room(props) {
               />
             </Form.Group>
           </Form>
-
-        }
+        )}
       </div>
     </>
   );
---
+++ src/components/QuestionCreator.js
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import { db, auth } from "../firebase";
+import dayjs from "dayjs";
+import { v4 as uuidv4 } from "uuid";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+function QuestionCreator(props) {
+  const [questionType, setQuestionType] = useState("free");
+  const [questionText, setQuestionText] = useState("");
+  const [optionText, setOptionText] = useState("");
+  const [options, setOptions] = useState([]);
+  const [user, loading] = useAuthState(auth);
+
+  function addOption(e) {
+    e.preventDefault();
+    setOptions((prev) =>
+      prev.concat({
+        optionId: uuidv4(),
+        text: optionText,
+      })
+    );
+    setOptionText("");
+  }
+
+  function postQuestion(e) {
+    e.preventDefault();
+    db.ref(`rooms/${props.roomId}/questions/${uuidv4()}`).set({
+      uid: user.uid,
+      text: questionText,
+      type: questionType,
+      reactionCount: 0,
+      askedTime: dayjs().format(),
+      userName: user?.displayName || "Anonymous",
+      options: options,
+      answers: [],
+    });
+    props.onQuestionSubmit();
+  }
+
+  return (
+    <>
+      <Form id="new-question-form" onSubmit={postQuestion}>
+        <Form.Group controlId="question-text" className="mb-3">
+          <Form.Control
+            required={true}
+            as="textarea"
+            autoComplete="off"
+            size="lg"
+            type="text"
+            className="bg-grey"
+            placeholder="Type a question..."
+            value={questionText}
+            onChange={(e) => setQuestionText(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <Button
+        size="sm"
+        variant={questionType === "free" ? "secondary" : "light"}
+        className="me-2"
+        onClick={() => setQuestionType("free")}
+      >
+        Free Text
+      </Button>
+      <Button
+        size="sm"
+        variant={questionType === "multi" ? "secondary" : "light"}
+        onClick={() => setQuestionType("multi")}
+      >
+        Multiple Choice
+      </Button>
+      {questionType === "multi" && (
+        <Form onSubmit={addOption} className="mt-3">
+          <ListGroup className="mb-3">
+            {options?.map((a) => (
+              <ListGroup.Item action type="button" key={a.optionId}>
+                {a.text}
+              </ListGroup.Item>
+            ))}
+            <ListGroup.Item action type="button">
+              <Form.Group controlId="option-text">
+                <Form.Control
+                  autoComplete="off"
+                  size="sm"
+                  type="text"
+                  className="bg-grey"
+                  placeholder="Type an option..."
+                  value={optionText}
+                  onChange={(e) => setOptionText(e.target.value)}
+                />
+              </Form.Group>
+            </ListGroup.Item>
+          </ListGroup>
+        </Form>
+      )}
+      <div>
+        <Button
+          variant="secondary"
+          form="new-question-form"
+          type="submit"
+          className="float-end"
+        >
+          Ask Question
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default QuestionCreator;
